1d780fba7470ecf2dcb50bc3a78e353be0399e04
Rafa Canosa <rafacv23@gmail.com>
2024-03-20 12:53:58 +0100

    methods to consum the api of supabase

---
+++ src/lib/api.js
@@ -0,0 +1,55 @@
+import { supabase } from "./supabase"
+
+export const getCategories = async () => {
+  let { data: categorias, error } = await supabase
+    .from("categorias")
+    .select("*")
+
+  if (error) {
+    console.error(error)
+  }
+
+  console.log(categorias)
+  return categorias
+}
+
+export const getArticlesByType = async (type) => {
+  // función que recoge los artículos de la api en función del tipo que estos tengan asignado.
+
+  let { data: articulos, error } = await supabase
+    .from("articulos")
+    .select("*")
+    .eq("type", type)
+
+  if (error) {
+    console.log(error)
+  }
+
+  console.log(articulos)
+  return articulos
+}
+
+export const getArticles = async () => {
+  let { data: articulos, error } = await supabase.from("articulos").select("*")
+
+  if (error) {
+    console.log(error)
+  }
+
+  console.log(articulos)
+  return articulos
+}
+
+export const getAuthorById = async (id) => {
+  let { data: redactores, error } = await supabase
+    .from("redactores")
+    .select("*")
+    .eq("id", id)
+
+  if (error) {
+    console.log(error)
+  }
+
+  console.log(redactores)
+  return redactores
+}
