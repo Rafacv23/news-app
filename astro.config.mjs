07f2d9eed86a4fcb1f41777143a9f9f08e77dcdd
Rafa Canosa <rafacv23@gmail.com>
2024-03-21 14:45:02 +0100

    prefetch enable for tap inputs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,8 @@ import { defineConfig } from "astro/config"
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  prefetch: true,
+  prefetch: {
+    defaultStrategy: "tap",
+  },
 })
