76931b0882bd8f9dd22ebb20f8520a148fdae49b
Rafa Canosa <rafacv23@gmail.com>
2024-03-25 17:36:33 +0100

    added astro adaptar for vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,5 +10,6 @@ export default defineConfig({
   prefetch: {
     defaultStrategy: "tap"
   },
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  adapter: vercel()
 });
