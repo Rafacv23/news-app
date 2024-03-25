bdb6b2fb6b645d24aeab1fe9c29053e1faa7f380
Rafa Canosa <rafacv23@gmail.com>
2024-03-25 13:19:46 +0100

    added preline ui

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "node_modules/preline/dist/*.js",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [require("rippleui")],
+  plugins: [require("rippleui"), require("preline/plugin")],
 }
