354fb002dd9b4ee3340057e352942a54c473161f
Rafa Canosa <rafacv23@gmail.com>
2024-03-25 16:51:47 +0100

    Json to storage the staff members

---
+++ src/lib/equipo.js
@@ -0,0 +1,31 @@
+export const equipo = [
+  {
+    nombre: "Rafa",
+    apellidos: "Canosa",
+    descripcion: "descripcion",
+    puesto: "CEO, diseñador y programador.",
+    avatar: "/favicon.avif",
+    rrss: [
+      {
+        nombre: "Twitter",
+        url: "https://twitter.com/rafa_canosa",
+        logo: "https://cdn-icons-png.flaticon.com/128/3256/3256013.png",
+      },
+      {
+        nombre: "Instagram",
+        url: "https://www.instagram.com/rafa_canosa_/",
+        logo: "https://cdn-icons-png.flaticon.com/128/2111/2111463.png",
+      },
+      {
+        nombre: "Linkedin",
+        url: "https://www.linkedin.com/in/rafa-canosa-vallejo-6328a5194/",
+        logo: "https://cdn-icons-png.flaticon.com/128/3536/3536505.png",
+      },
+      {
+        nombre: "Github",
+        url: "https://github.com/Rafacv23",
+        logo: "https://cdn-icons-png.flaticon.com/128/5968/5968866.png",
+      },
+    ],
+  },
+]
