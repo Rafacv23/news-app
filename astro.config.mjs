fad04ccabeea43e63aabf474057ae31bd3b5e3a3
Rafa Canosa <rafacv23@gmail.com>
2024-03-28 14:36:23 +0100

    vercel static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,21 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
-
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import sitemap from "@astrojs/sitemap"
+import vercelStatic from "@astrojs/vercel/static"
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   prefetch: {
-    defaultStrategy: "tap"
+    defaultStrategy: "tap",
   },
   site: "https://taberna-precursor.vercel.app/",
-  integrations: [tailwind(), sitemap({
-    filter: (page) => page !== 'https://taberna-precursor.vercel.app/test/',
-  })],
-});
+  integrations: [
+    tailwind(),
+    sitemap({
+      filter: (page) => page !== "https://taberna-precursor.vercel.app/test/",
+    }),
+  ],
+  output: "static",
+  adapter: vercelStatic(),
+})
