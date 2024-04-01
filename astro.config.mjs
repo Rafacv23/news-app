eb1253afabdb44199b0c5af5c660f27b0a9c7d02
Rafa Canosa <rafacv23@gmail.com>
2024-04-01 13:47:41 +0200

    added react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,20 @@
-import { defineConfig } from "astro/config"
-import tailwind from "@astrojs/tailwind"
-import sitemap from "@astrojs/sitemap"
-import vercelStatic from "@astrojs/vercel/static"
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import vercelStatic from "@astrojs/vercel/static";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   prefetch: {
-    defaultStrategy: "tap",
+    defaultStrategy: "tap"
   },
   site: "https://taberna-precursor.vercel.app/",
-  integrations: [
-    tailwind(),
-    sitemap({
-      filter: (page) => page !== "https://taberna-precursor.vercel.app/test/",
-    }),
-  ],
+  integrations: [tailwind(), sitemap({
+    filter: page => page !== "https://taberna-precursor.vercel.app/test/"
+  }), react()],
   output: "static",
-  adapter: vercelStatic(),
-})
+  adapter: vercelStatic()
+});
